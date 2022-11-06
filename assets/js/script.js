ef40d741df174454369d5a817ede9a7a27f8e7a0
Ahmed Alasow <a.alasow01@gmail.com>
2022-11-06 11:51:26 +0000

    Added getlocation, findlocation and displaycurrentweather functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-const weatherURL = 'https://api.openweathermap.org/';
-const weatherAPIKEY = '272b68b95d1c42ff7655c2f715fa4879';
+const WEATHER_BASE_API_URL = 'https://api.openweathermap.org';
+const WEATHER_API_KEY = '272b68b95d1c42ff7655c2f715fa4879';
 const maxForecast = 5;
 
+const weatherLocation = document.getElementById('weatherLocation');
+const searchBtn = document.getElementById('search');
 
+
+// Returns the location once the user has submitted one
 function getLocation(){
 
     var userInput = weatherLocation.value;
-    if(userInput == ''){
+    if(userInput === ''){
         window.alert("Please enter a location");
     }else{
         findLocation(weatherLocation);
@@ -14,105 +18,38 @@ function getLocation(){
 
 }
 
-
-function findLocation(userInput){
+// Finds the location the the user has submitted
+function findLocation(search){
 
     // the URL are now var variables so that they can be updated globally throughout the application
-    var locationURL = `${weatherURL}/geo/1.0/direct?q={userInput}&limit=5&appid=${weatherAPIKEY}`;
+    var apiURL = `${WEATHER_BASE_API_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`;
 
-    fetch(locationURL)
+    fetch(apiURL)
         .then(function (response){
-            return response.json;
+            return response.json();
         })
         .then(function (data){
+            // Stores the location information into an array for developers to see in the console
+            var myLocation = data[0];
 
-            // Finds the first location from the list based of what the user has submitted 
-            const location = data[0]
+            console.log("Pressed");
 
-            console.log(location);
+            console.log(myLocation);
 
-            displayWeather(location);
+
         })
     
 }
 
-
+// Displays the current weather for the location based on the day the user uses the website
 function displayCurrentWeather(weatherData){
 
-    var currentWeather = weatherData.current;
+    const currentWeather = weatherData.current;
 
     document.getElementById('temperature-value').textContent = 'Temperature: ' + `${currentWeather.temp}` + ' °C';
     document.getElementById('humidity-value').textContent = "Humidity :" + `${currentWeather.humidity}` + ' %';
     document.getElementById('wind-speed-value').textContent = 'Wind Speed: '  + `${currentWeather.wind-speed}` + ' km/h';
 
 }
-
-function displayWeatherForecast(weatherData){
-
-    // Contains access to daily forecasts
-    const daily = weatherData.daily;
-
-    document.getElementById('forecast').style.display = 'block';
-
-    const forecastList = document.getElementById('forecast-list');
-    forecastList.innerHTML = '';
-
-    for(var i = 0; i < maxForecast; i++){
-        const dailyForecast = daily[i];
-        const day = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB' ,{weekday: 'long'})
-        const dailyTemp = 'Temperature: ' + `${currentWeather.temp}` + ' °C';
-        const dailyHumidity = "Humidity :" + `${currentWeather.humidity}` + ' %';
-        const dailyWindSpeed = 'Wind Speed: '  + `${currentWeather.wind-speed}` + ' km/h';
-
-        
-        const nextForecast = document.createElement('div');
-        nextForecast.classList.add('forecast-day');
-        nextForecast.innerHTML = `<div class="weather-info">
-        <div class=date>
-            <span>${date}</span>
-        </div>
-        <div class="temperature">
-            <span>${temp}</span>
-        </div>
-
-        <div class="humidity">
-            <span>${humidity}</span>
-        </div>
-
-        <div class="wind-speed">
-            <span>${wind-speed}</span>
-        </div>
-    </div>`
-
-        forecastList.appendChild(nextForecast);
-    }
-
-}
-
-// Returns the weather based on the locations latitude and longitude
-function getWeather(lat, lon){
-    var weatherURL = `${weatherURL}/geo/1.0/reverse?lat={lat}&lon={lon}&limit=5&appid=${weatherAPIKEY}`;
-    fetch(weatherURL)
-        .then(function(response){
-            return response.json;
-        })
-        .then(function(data){
-
-            // displays the weather of the current day the user has submitted
-            displayCurrentWeather(data);
-
-            // displays the next five days weather
-            displayWeatherForecast(data);
-        })
-}
-
-function displayWeather(weatherData){
-    document.getElementById('weatherLocation').textContent = `$(weatherData.name), $(weatherData.country)`;
-
-    getWeather(weatherData.lat, weatherData.lon)
-}
-
-const weatherLocation = document.getElementById('weatherLocation');
-const searchBtn = document.getElementById('search');
 
 searchBtn.addEventListener('click', getLocation);
